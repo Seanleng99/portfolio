2bda828c5a36bd4728266bd3dec9a87ce2220ed5
Seanleng99 <seanleng99@gmail.com>
2022-01-05 19:32:01 +0800

    fix menu button style

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -19,10 +19,12 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-            <span className="line1"></span>
-            <span className="line2"></span>
-            <span className="line3"></span>
+          <div className="menuBtn" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className="hamburger">
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
           </div>
         </div>
       </div>
